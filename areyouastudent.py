6258a1f0b3da9a27c80121f18762e57661342172
Keith Benjamin <kjb528i@yahoo.com>
2022-04-26 19:53:30 -0400

    added employee checker bonus

--- areyouastudent.py
+++ areyouastudent.py
@@ -2,9 +2,9 @@ print('Welcome to the CareerDevs employee check-in.')
 
 careerdevs = ["Keith", "Manny", "Rodney", "Kat"]
 print("\nAre you a student? Lets find out...\n")
-accept = "";
+accept = ""
 while accept == "":
-    accept = input("Are you a student?\n(yes/no): ").strip()
+    accept = input("Are you a student?\n(yes/no): ").strip().lower()
 if accept.startswith("y"):
     name = input("What is your name?\n").strip()
     for emp_name in careerdevs:
@@ -13,6 +13,5 @@ if accept.startswith("y"):
             break
     else:
         print("You're not suppose to be here!")
-
 else:
     print ("Okay, you can leave!!")
