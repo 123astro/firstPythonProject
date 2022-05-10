6a6d2f86e2ae0eb4aa9b9c68f39bfc8c581a8f9f
Keith Benjamin <kjb528i@yahoo.com>
2022-05-09 21:56:57 -0400

    made an adjustment to different_max_min_nums_in_list.py

--- different_max_min_nums_in_list.py
+++ different_max_min_nums_in_list.py
@@ -1,9 +1,10 @@
-num_list = [17, 4, -1, 2, 8, -100]
+num_list = [199, 23, -100, 2, 8, 100]
 
 
 def difference_max_min(lst):
     num_high = num_list[0]
     num_low = num_list[0]
+    # for loop starts comparing index 1 with index 0
     for count, x in enumerate(lst, start=1):
         if x < num_low:
             num_low = x
